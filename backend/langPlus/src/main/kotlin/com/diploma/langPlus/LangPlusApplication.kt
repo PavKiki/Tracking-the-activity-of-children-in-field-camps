3dfc49ae8bed25d817f9b35f4a2ff3a6ccf663fa
Pavel <pavelgren105@outlook.com>
2023-03-29 23:42:04 +0300

    v1.0.1 (backend groundwork)

---
+++ backend/langPlus/src/main/kotlin/com/diploma/langPlus/LangPlusApplication.kt
@@ -0,0 +1,11 @@
+package com.diploma.langPlus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LangPlusApplication
+
+fun main(args: Array<String>) {
+	runApplication<LangPlusApplication>(*args)
+}
---
+++ backend/langPlus/src/test/kotlin/com/diploma/langPlus/LangPlusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diploma.langPlus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LangPlusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
