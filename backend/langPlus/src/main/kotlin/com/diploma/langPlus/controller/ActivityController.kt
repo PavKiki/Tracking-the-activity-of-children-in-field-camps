05b50ebf7b741f8f4a574200702d12229fa94869
Pavel <pavelgren105@outlook.com>
2023-04-03 23:55:22 +0300

    v1.0.5 (solid backend groundwork)

--- backend/langPlus/src/main/kotlin/com/diploma/langPlus/controller/ActivityController.kt
+++ backend/langPlus/src/main/kotlin/com/diploma/langPlus/controller/ActivityController.kt
@@ -3,11 +3,14 @@ package com.diploma.langPlus.controller
 import com.diploma.langPlus.dto.ActivityDto
 import com.diploma.langPlus.entity.toDto
 import com.diploma.langPlus.service.ActivityService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/activities")
+@RequestMapping("/activity")
 class ActivityController(val activityService: ActivityService) {
+    @GetMapping
     fun getAll(): List<ActivityDto> = activityService.getAll().map { it.toDto() }
 }
--- backend/langPlus/src/main/kotlin/com/diploma/langPlus/controller/TimetableController.kt
+++ backend/langPlus/src/main/kotlin/com/diploma/langPlus/controller/TimetableController.kt
@@ -3,11 +3,13 @@ package com.diploma.langPlus.controller
 import com.diploma.langPlus.dto.TimetableDto
 import com.diploma.langPlus.entity.toDto
 import com.diploma.langPlus.service.TimetableService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/timetable")
 class TimetableController (private val timetableService: TimetableService) {
+    @GetMapping
     fun getAll(): List<TimetableDto> = timetableService.getAll().map { it.toDto() }
 }
